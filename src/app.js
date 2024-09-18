1cf0fcf90e63620a55d6397780a836fa3c8bece1
Anant Verma <E088088@mcgladrey.rsm.net>
2024-09-18 23:35:55 +0530

    created a server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/hello", (req, res) => {
+  res.send("Hello hello hello");
+});
+app.use("/test", (req, res) => {
+  res.send("test test test");
+});
+app.use("/", (req, res) => {
+  res.send("N from the dashboard");
+});
+
+app.listen(7777, () => {
+  console.log("Server is successfully listening on port 7777");
+});
