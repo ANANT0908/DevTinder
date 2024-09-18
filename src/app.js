0db944b4e7fc4e134caa89efa4e4c607231582a8
Anant Verma <E088088@mcgladrey.rsm.net>
2024-09-19 00:32:10 +0530

    exploring get , post ,delete calls

--- src/app.js
+++ src/app.js
@@ -2,20 +2,20 @@ const express = require("express");
 
 const app = express();
 
-
-app.use("/hello/2", (req, res) => {
-  res.send("Abracadabra!");
+app.get("/user",(req, res) => {
+  res.send({firstname:"Anant", lastname:"Verma"});
+});
+app.post("/user",(req, res) => {
+  res.send("Data successfully saved to the database");
+});
+app.delete("/user",(req, res) => {
+  res.send("Deleted successfully");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello");
-});
 app.use("/test", (req, res) => {
   res.send("test test test");
 });
-app.use("/", (req, res) => {
-  res.send("N from the dashboard");
-});
+
 
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777");
