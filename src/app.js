9571dc3153176a6fa732b51033bc138319d47f47
Anant Verma <E088088@mcgladrey.rsm.net>
2024-09-19 00:43:41 +0530

    Dynamic Routes

--- src/app.js
+++ src/app.js
@@ -2,19 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user",(req, res) => {
+app.get("/user/:userId",(req, res) => {
+  console.log(req.params);
   res.send({firstname:"Anant", lastname:"Verma"});
 });
-app.post("/user",(req, res) => {
-  res.send("Data successfully saved to the database");
-});
-app.delete("/user",(req, res) => {
-  res.send("Deleted successfully");
+// app.get("/user",(req, res) => {
+//   console.log(req.query);
+//   res.send({firstname:"Anant", lastname:"Verma"});
+// });
+
+
+app.get("/a(bc)?d",(req, res) => {
+  res.send({firstname:"Anant", lastname:"Verma"});
 });
 
-app.use("/test", (req, res) => {
-  res.send("test test test");
-});
+
 
 
 app.listen(7777, () => {
