ff20a0d9ecdde0f03d1e3d5c81045b9f001a3ce1
Anant Verma <E088088@mcgladrey.rsm.net>
2024-09-20 00:27:00 +0530

    Error Handling

--- src/app.js
+++ src/app.js
@@ -2,24 +2,35 @@ const express = require("express");
 
 const app = express();
 
-const {adminAuth, userAuth} = require("./middlewares/auth");
+// const {adminAuth, userAuth} = require("./middlewares/auth");
 
-app.use("/admin", adminAuth);
+// app.use("/admin", adminAuth);
 
-app.get("/user/login", (req, res) => {
-  res.send("User Login");
-});
-app.get("/user",userAuth, (req, res) => {
-  res.send("User Data sent");
+// app.get("/user/login", (req, res) => {
+//   res.send("User Login");
+// });
+// app.get("/user",userAuth, (req, res) => {
+//   res.send("User Data sent");
+// });
+
+// app.get("/admin/getAllData", (req, res) => {
+//   res.send("All Data sent");
+// });
+// app.get("/admin/deleteUser", (req, res) => {
+//   res.send("Deleted a user");
+// });
+
+
+app.get("/getUserData", (req, res) => {
+  throw new Error("dsalkfa");
+  res.send("Sent User Data");
 });
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All Data sent");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted a user");
-});
-
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 7777");
 });
