f50751807cd0cf443405333cd0602b2868fff1de
Anant Verma <E088088@mcgladrey.rsm.net>
2024-09-22 01:13:23 +0530

    validators

--- src/modals/user.js
+++ src/modals/user.js
@@ -1,42 +1,59 @@
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true, minLength: 4, maxLength: 50 },
-  lastName: { type: String },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: { type: String, required: true },
-  age: {
-    type: Number,
-    min: 18
-  },
-  gender: {
-    type: String,
-    validate(value){
-      if(!['male', 'female', 'other'].includes(value)){
-        throw new Error('Gender data is not valid')
+const validator = require("validator");
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true, minLength: 4, maxLength: 50 },
+    lastName: { type: String },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email Address");
+        }
+      },
+    },
+    password: { type: String, required: true, validate(value){
+      if(!validator.isStrongPassword(value)){
+        throw new Error("Enter a Strong Password");
       }
-    }
+    } },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://picsum.photos/200/300",
+      validate(value){
+        if(!validator.isURL(value)){
+          throw new Error("Invalid Photo URL");
+        }
+      }
+    },
+    about: {
+      type: String,
+      default: "This is a default about of user!",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  photoUrl: {
-    type: String,
-    default: "default Url",
-  },
-  about: {
-    type: String,
-    default: "This is a default about of user!",
-  },
-  skills: {
-    type: [String],
-  },
-},{
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
