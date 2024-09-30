5bc19aa11d4ae26ab72d480e059cc5dea959f97f
Anant Verma <E088088@mcgladrey.rsm.net>
2024-10-01 00:29:39 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -2,19 +2,22 @@ const express = require("express");
 const connectDB = require("./cofig/database");
 const app = express();
 const cookieParser = require("cookie-parser");
- const authRouter = require("./routes/auth")
- const profileRouter = require("./routes/profile")
- const requestRouter = require("./routes/request");
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const cors = require("cors");
 
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
-app.use("/",authRouter)
-app.use("/",profileRouter)
-app.use("/",requestRouter)
-app.use("/",userRouter)
-
-
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
 
 connectDB()
   .then(() => {
